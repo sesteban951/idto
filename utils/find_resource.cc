227746fba5a3204f555b19bceaee67226e23cc08
sesteban951 <sesteban@caltech.edu>
2025-06-10 17:49:42 -0700

    commednted out requirement path assertion in find_resources

--- utils/find_resource.cc
+++ utils/find_resource.cc
@@ -8,10 +8,10 @@ namespace idto {
 namespace utils {
 
 std::string FindIdtoResource(const std::string& idto_resource_path) {
-  if (idto_resource_path.substr(0, 5) != "idto/") {
-    throw std::runtime_error(fmt::format(
-        "Resource path '{}' must start with 'idto/' ", idto_resource_path));
-  }
+  // if (idto_resource_path.substr(0, 5) != "idto/") {
+  //   throw std::runtime_error(fmt::format(
+  //       "Resource path '{}' must start with 'idto/' ", idto_resource_path));
+  // }
   // N.B. IDTO_BINARY_DIR set in top-level CMakeLists.txt
   return std::string(IDTO_BINARY_DIR) + "/" + idto_resource_path;
 }
